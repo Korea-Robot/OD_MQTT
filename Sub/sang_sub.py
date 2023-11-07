fdfb2765559bf31a6b18c6ce3424debf024a41ab
sim <sim30217@gmail.com>
2023-11-07 17:31:52 +0900

    add voice

--- Sub/sang_sub.py
+++ Sub/sang_sub.py
@@ -1,17 +1,30 @@
-# main_subscriber.py
 import json
+import pygame
 from mqtt_subscriber import MQTT_SUB
 from database import Database
 from config import DBConfig
 
 # Initialize database
-db = Database(DBConfig)
+#db = Database(DBConfig)
+
+# Initialize the mixer module
+pygame.mixer.init()
 
 # Callback when a PUBLISH message is received from the server
 def on_message(client, userdata, msg):
     print(f"Received message on {msg.topic}: {msg.payload.decode('utf-8')}")
     payload_data = json.loads(msg.payload)
-    db.insert_detection(payload_data)
+    #db.insert_detection(payload_data)
+    
+    # Check if "keyboard" is in the message and play sound if it is
+    if 'keyboard' in payload_data:
+        try:
+            pygame.mixer.music.load('speech_eng.mp3')  # Replace with your MP3 file path
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():  # Wait for the music to finish playing
+                pygame.time.Clock().tick(10)
+        except Exception as e:
+            print(f"Error playing sound: {e}")
 
 # Initialize MQTT client with the defined callback function
 mqtt_sub = MQTT_SUB(on_message=on_message)
@@ -26,4 +39,4 @@ except KeyboardInterrupt:
 finally:
     # Clean up resources
     mqtt_sub.close()
-    db.close()
+    #db.close()
